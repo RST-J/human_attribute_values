9ae365e2e3f3a6f88fce754aaa56e858072635e3
Jonas Peschla <github@peschla.net>
2018-08-28 00:07:32 +0200

    Update tested ruby and rails versions

--- Appraisals
+++ Appraisals
@@ -1,19 +1,19 @@
-appraise "rails-4.1.8" do
-  gem "rails", "4.1.8"
+appraise "rails-4.1.16" do
+  gem "rails", "4.1.16"
 end
 
-appraise "rails-4.1.10" do
-  gem "rails", "4.1.10"
-end
-
-appraise "rails-4.2.1" do
-  gem "rails", "4.2.1"
+appraise "rails-4.2.10" do
+  gem "rails", "4.2.10"
 end
 
 appraise "rails-5.0.0.1" do
   gem "rails", "5.0.0.1"
 end
 
-appraise "rails-5.1" do
-  gem "rails", "5.1"
+appraise "rails-5.1.6" do
+  gem "rails", "5.1.6"
+end
+
+appraise "rails-5.2.1" do
+  gem "rails", "5.2.1"
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,44 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131170613) do
+ActiveRecord::Schema.define(version: 2015_01_31_170613) do
 
-  create_table "boolean_models", force: true do |t|
-    t.boolean  "boolean_field"
+  create_table "boolean_models", force: :cascade do |t|
+    t.boolean "boolean_field"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "enum_models", force: true do |t|
-    t.integer  "status"
+  create_table "enum_models", force: :cascade do |t|
+    t.integer "status"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "lexicons", force: true do |t|
-    t.integer  "the_answer_id"
+  create_table "lexicons", force: :cascade do |t|
+    t.integer "the_answer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["the_answer_id"], name: "index_lexicons_on_the_answer_id"
+  end
+
+  create_table "numeric_models", force: :cascade do |t|
+    t.integer "integer_field"
+    t.decimal "decimal_field"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "lexicons", ["the_answer_id"], name: "index_lexicons_on_the_answer_id"
-
-  create_table "numeric_models", force: true do |t|
-    t.integer  "integer_field"
-    t.decimal  "decimal_field"
+  create_table "parents", force: :cascade do |t|
+    t.string "type"
+    t.string "field"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "parents", force: true do |t|
-    t.string   "type"
-    t.string   "field"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "the_answers", force: true do |t|
-    t.string   "ultimate_truth"
+  create_table "the_answers", force: :cascade do |t|
+    t.string "ultimate_truth"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
