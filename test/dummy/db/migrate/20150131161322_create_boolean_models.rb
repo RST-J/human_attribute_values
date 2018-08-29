14517fcc6b2b3174cc2771987fd6d5900373cd1b
Jonas Peschla <github@peschla.net>
2018-08-29 22:34:57 +0200

    Use right superclass for rails >= 5.0

--- test/dummy/db/migrate/20150131161322_create_boolean_models.rb
+++ test/dummy/db/migrate/20150131161322_create_boolean_models.rb
@@ -1,4 +1,7 @@
-class CreateBooleanModels < ActiveRecord::Migration
+superclass = ActiveRecord::Migration
+superclass = ActiveRecord::Migration[5.1] if superclass.respond_to?(:[])
+
+class CreateBooleanModels < superclass
   def change
     create_table :boolean_models do |t|
       t.boolean :boolean_field
--- test/dummy/db/migrate/20150131162551_create_numeric_models.rb
+++ test/dummy/db/migrate/20150131162551_create_numeric_models.rb
@@ -1,4 +1,7 @@
-class CreateNumericModels < ActiveRecord::Migration
+superclass = ActiveRecord::Migration
+superclass = ActiveRecord::Migration[5.1] if superclass.respond_to?(:[])
+
+class CreateNumericModels < superclass
   def change
     create_table :numeric_models do |t|
       t.integer :integer_field
